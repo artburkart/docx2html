4b193993f745fa7cfbdfc3bb4c434f4c9beb3bbc
brian <brian@Nitori>
2014-02-23 01:16:42 -0500

    added a getParagraphs function

--- docx.js
+++ docx.js
@@ -1,32 +1,73 @@
-var fs = require('fs');
-var obj = JSON.parse(fs.readFileSync("./document.json", 'utf8'));
+var unzip = require('./unzipDocx');
+var body;
 
-var par = obj["w:document"]["w:body"]["w:p"];
+unzip.getBody('./nom.docx', function(data, err) {
+  if (err) {
+    console.dir(err);
+  }
+  else {
+    body = data;
+  }
+});
 
-for (var key in par) {
-  if ('w:r' in par[key]) {
-    //console.log(par[key]['w:r']);
-    if (Object.prototype.toString.call(par[key]['w:r']) === '[object Array]') {
-      for (var k in par[key]['w:r']) {
-        if (typeof(par[key]['w:r'][k]['w:t']) === "string") {
-          console.log(par[key]['w:r'][k]['w:t']);
-        } 
-        else {
-          console.log(par[key]['w:r'][k]['w:t']);//["#text"]);
-        }
-      }
-    }
-    else if ('w:t' in par[key]['w:r']) {
-      //console.log(typeof(par[key]['w:r']['w:t']));
-      if (typeof(par[key]['w:r']['w:t']) === "string") {
-        console.log(par[key]['w:r']['w:t']);
-      }
-      else {
-        console.log(par[key]['w:r']['w:t']['#text']);
-      }
+//var body = obj["w:document"]["w:body"]["w:p"];
+
+getParagraphs = function(data) {
+  pars = []
+  for (var key in data) {
+    if ('w:r' in data[key]) {
+      pars.push(data[key]['w:r']);
     }
   }
-//  else {
-//    console.log("NO MATCH!\n" + par[key] + "\n");
-//  }
+  return pars;
 }
+
+exports.getParagraphs = getParagraphs;
+
+//var paragraphs = getParagraphs(body);
+//console.log(paragraphs);
+
+//for (var key in body) {
+//  console.log(JSON.stringify(body[key]));
+//  console.log("\n");
+//  //if ('w:r' in body[key] && 'w:t' in body[key]['w:r'][0]) {
+//  //  txt = body[key]['w:r'][0]['w:t'][0];
+//  //  console.log(txt);
+//  //  //if (txt === 'string') {
+//  //  //  console.log(txt);
+//  //  //}
+//  //  //else {
+//  //  //  console.log(txt['$']);
+//  //  //}
+//  //  //else {
+//  //  //  console.log(body[key]['w:r']);
+//  //  //}
+//  //}
+//  //else {
+//  //  console.log("w:r not a key!\t" + body[key] + "\n");
+//  //}
+//    //console.log(body[key]['w:r']);
+//  //  if (Object.prototype.toString.call(body[key]['w:r']) === '[object Array]') {
+//  //    for (var k in body[key]['w:r']) {
+//  //      if (typeof(body[key]['w:r'][k]['w:t']) === "string") {
+//  //        console.log(body[key]['w:r'][k]['w:t']);
+//  //      } 
+//  //      else {
+//  //        console.log(body[key]['w:r'][k]['w:t']);//["#text"]);
+//  //      }
+//  //    }
+//  //  }
+//  //  else if ('w:t' in body[key]['w:r']) {
+//  //    //console.log(typeof(body[key]['w:r']['w:t']));
+//  //    if (typeof(body[key]['w:r']['w:t']) === "string") {
+//  //      console.log(body[key]['w:r']['w:t']);
+//  //    }
+//  //    else {
+//  //      console.log(body[key]['w:r']['w:t']['#text']);
+//  //    }
+//  //  }
+//  //}
+////  else {
+////    console.log("NO MATCH!\n" + body[key] + "\n");
+////  }
+//}
