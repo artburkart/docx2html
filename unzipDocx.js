fc447e72738c85c5949350237d06ade138e2b1db
Arthur <artburkart@gmail.com>
2014-02-23 00:39:32 -0500

    one more refactory for unzip

--- unzipDocx.js
+++ unzipDocx.js
@@ -14,16 +14,16 @@ getBody = function(path, callback) {
         var zipEntries = zip.getEntries();
 
         // iterate through documents
-        _.each(zipEntries, function(zipEntry) {
-            if (zipEntry.entryName == "word/document.xml")
-                parser.parseString(zipEntry.getData(), function(err, result) {
-                    if (err) callback(undefined, err);
-                    else {
-                        var body = result['w:document']['w:body'][0]['w:p'];
-                        callback(body, undefined);
-                    }
-                });
+        var entry = _.findWhere(zipEntries, {entryName: "word/document.xml"});
+
+        parser.parseString(entry.getData(), function(err, result) {
+            if (err) return callback(undefined, err);
+            else {
+                var body = result['w:document']['w:body'][0]['w:p'];
+                return callback(body, undefined);
+            }
         });
+        return callback(undefined, "Error: not a valid docx file.");
     });
 }
 
