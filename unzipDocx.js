14588eba7bc9aaff1eb058b7e3854f98d5d549a2
Arthur <artburkart@gmail.com>
2014-02-23 00:12:28 -0500

    made unzipDocx into a function

--- unzipDocx.js
+++ unzipDocx.js
@@ -1,14 +1,27 @@
 var AdmZip = require('adm-zip'),
 parser = require('xml2js').Parser();
 
-// unzip docx and get each entry
-var zip = new AdmZip("./alit.docx");
-var zipEntries = zip.getEntries();
+// unzip docx and get body text
+getBody = function(path, callback) {
+    var zip = new AdmZip(path);
+    var zipEntries = zip.getEntries();
 
-// iterate through documents
-zipEntries.forEach(function(zipEntry) {
-    if (zipEntry.entryName == "word/document.xml")
-        parser.parseString(zipEntry.getData(), function(err, result) {
-            console.dir(result['w:document']['w:body'][0]['w:p']);
-        });
-});
+    // iterate through documents
+    zipEntries.forEach(function(zipEntry) {
+        if (zipEntry.entryName == "word/document.xml")
+            parser.parseString(zipEntry.getData(), function(err, result) {
+                if (err) callback(undefined, err);
+                else {
+                    var body = result['w:document']['w:body'][0]['w:p'];
+                    callback(body, undefined);
+                }
+            });
+    });
+}
+
+exports.getBody = getBody;
+
+// getBody("./alit.docx", function(data, err) {
+//     if (err) console.dir(err);
+//     else console.dir(data);
+// });
