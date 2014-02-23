de80d7a531c67c4ec3f8ff8df8a21ca85e6f1d89
Arthur <artburkart@gmail.com>
2014-02-23 00:33:06 -0500

    fixed the getBody function

--- unzipDocx.js
+++ unzipDocx.js
@@ -1,28 +1,35 @@
 var AdmZip = require('adm-zip'),
 parser = require('xml2js').Parser(),
-_ = require('underscore');
+_ = require('underscore'),
+fs = require('fs');
 
 // unzip docx and get body text
 getBody = function(path, callback) {
-    var zip = new AdmZip(path);
-    var zipEntries = zip.getEntries();
 
-    // iterate through documents
-    _.each(zipEntries, function(zipEntry) {
-        if (zipEntry.entryName == "word/document.xml")
-            parser.parseString(zipEntry.getData(), function(err, result) {
-                if (err) callback(undefined, err);
-                else {
-                    var body = result['w:document']['w:body'][0]['w:p'];
-                    callback(body, undefined);
-                }
-            });
+    // path validation
+    fs.exists(path, function (exists) {
+        if (!exists) return callback(undefined, "Error: file does not exist.");
+
+        var zip = new AdmZip(path);
+        var zipEntries = zip.getEntries();
+
+        // iterate through documents
+        _.each(zipEntries, function(zipEntry) {
+            if (zipEntry.entryName == "word/document.xml")
+                parser.parseString(zipEntry.getData(), function(err, result) {
+                    if (err) callback(undefined, err);
+                    else {
+                        var body = result['w:document']['w:body'][0]['w:p'];
+                        callback(body, undefined);
+                    }
+                });
+        });
     });
 }
 
 exports.getBody = getBody;
 
-// getBody("./alit.docx", function(data, err) {
+// getBody("./nom.docx", function(data, err) {
 //     if (err) console.dir(err);
 //     else console.dir(data);
 // });
